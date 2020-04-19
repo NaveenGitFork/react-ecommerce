9fe5e33b95f12f09f604bf01a3f86cd1d5a4348f
naveen choudhary <naveen.choudhary@optus.com.au>
2020-04-19 10:00:29 +1000

    integrated firebase into application and sign in page and google

--- src/App.js
+++ src/App.js
@@ -5,18 +5,40 @@ import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
 import Header from "./components/header/Header";
 import SignInSignOut from "./pages/signinsignout/SignInSignOut";
+import { auth } from "./firebase/firebase.util";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signIn" component={SignInSignOut} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signIn" component={SignInSignOut} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
