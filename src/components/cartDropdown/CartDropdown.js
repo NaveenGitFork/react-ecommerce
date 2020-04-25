87d8bd71ef9fb39783aee33428f2cdfff5a4922f
naveen choudhary <naveen.choudhary@optus.com.au>
2020-04-26 08:56:21 +1000

    on click of add to cart code to add item in shopping cart

--- src/components/cartDropdown/CartDropdown.js
+++ src/components/cartDropdown/CartDropdown.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./CartDropdown.style.scss";
 import CustomButton from "../custombutton/CustomButton";
+import CartItem from "../cartItem/CartItem";
+import { connect } from "react-redux";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartitems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartitems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartitems } }) => ({
+  cartitems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
